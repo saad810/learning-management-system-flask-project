d9e1ecd5b1fb4ef5f518d00b2f724c59cb74b95a
Saad Amin <93635982+saad810@users.noreply.github.com>
2023-09-05 01:19:17 +0500

    update btn.js

--- lms_app/static/js/btn.js
+++ lms_app/static/js/btn.js
@@ -1,11 +1,14 @@
-var flash_msg = document.getElementById('flash_msg');
+window.onload = flash_msg();
 
-if (flash_msg.innerText !== "") {
-    flash_msg.style.display = 'block';
+function flash_msg() {
+  var flash_msg = document.getElementById("flash_msg");
+  if (flash_msg.innerText !== "") {
+    flash_msg.style.display = "block";
 
-      setTimeout(function() {
-        flash_msg.style.display = 'none';
-    }, 10000); // 4000 milliseconds = 4 seconds
-}else{
-    flash_msg.style.display = none;
+    setTimeout(function () {
+      flash_msg.style.display = "none";
+    }, 5000); // 10000 milliseconds = 10 seconds
+  } else {
+    flash_msg.style.display = "none"; // Use "none" as a string
+  }
 }
